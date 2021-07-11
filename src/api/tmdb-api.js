256a9345f903e4fc0178ea458c90a5b17d0b68aa
John Gleeson <john.gleeson93@gmail.com>
2021-07-11 19:22:58 +0100

    Fixes

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -29,3 +29,12 @@ export const getMovies = () => {
       .then((res) => res.json())
       .then((json) => json.posters);
   };
+  export const getCountries = () => {
+    return fetch(
+      "https://api.themoviedb.org/3/countries/movie/list?api_key=" +
+        process.env.REACT_APP_TMDB_KEY +
+        "&language=en-US"
+    )
+      .then(res => res.json())
+      .then(json => json.countries);
+  };
