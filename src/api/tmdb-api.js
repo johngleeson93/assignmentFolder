aa23a7651712e499bad16c0c8e8ae83f7519aa47
John Gleeson <john.gleeson93@gmail.com>
2021-07-12 23:11:03 +0100

    Show movie review excerpts; Refactor Details page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -38,3 +38,13 @@ export const getMovies = () => {
       .then(res => res.json())
       .then(json => json.countries);
   };
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
