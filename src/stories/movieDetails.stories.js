abcd1adb6fd16858a17c5adfaeacea22641226e4
John Gleeson <john.gleeson93@gmail.com>
2021-07-15 22:10:34 +0100

    Fix broken stories in storybook

--- src/stories/movieDetails.stories.js
+++ src/stories/movieDetails.stories.js
@@ -5,7 +5,12 @@ import SampleMovie from "./sampleData";
 export default {
   title: "Movie Details Page/MovieDetails",
   component: MovieDetails,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+  ],
 };
 
 export const Basic = () => <MovieDetails movie={SampleMovie} />;
+
 Basic.storyName = "Default";
