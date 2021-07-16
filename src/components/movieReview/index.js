ef78b6390a6515e21bcd0b0aefbf7ebc3c1862bf
John Gleeson <john.gleeson93@gmail.com>
2021-07-16 21:50:20 +0100

    Update index.js

--- src/components/movieReview/index.js
+++ src/components/movieReview/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const MovieReview =  ({ review }) => {
-  return (
-    <>
-      <p>Review By: {review.author} </p>
-      <p>{review.content} </p>
-    </>
-  );
-};
-export default MovieReview
+const MovieReview = ({review}) => (
+
+  <>
+    <p>
+      Review By:
+      {review.author}
+    </p>
+    <p>
+      {review.content}
+      {' '}
+    </p>
+  </>
+);
+export default MovieReview;
